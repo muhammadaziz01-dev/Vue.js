80f8b99ef1596dab063612631e991377ab03ffcd
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-09-21 16:53:33 -1200

    lesson-3 amalyot loyihnin ui qismi tayorlandi

--- lesson-3/src/App.vue
+++ lesson-3/src/App.vue
@@ -1,30 +1,109 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="wrapper">
+      <form action="#">
+        <input type="text" placeholder="Enter task title" />
+        <button type="submit">add task</button>
+      </form>
+      <ul class="list">
+        <li class="list__itim">
+          <strong>1</strong>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <button class="delete">delete</button><button class="done">done</button>
+        </li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup></script>
+
+<style lang="scss" scoped>
+.wrapper {
+  max-width: 650px;
+  min-height: 60vh;
+  background-color: #edecec;
+  margin: 50px auto;
+  padding: 50px;
+  border-radius: 20px;
+  backdrop-filter: bulr(20px);
+
+  form {
+    display: flex;
+    gap: 5px;
+    input {
+      flex-grow: 2;
+      padding: 10px;
+      border: none;
+      border-radius: 12px 0 0 12px;
+      outline: none;
+      &:hover {
+        outline: 1px solid green;
+      }
+    }
+
+    button {
+      padding: 10px 20px;
+      background-color: #4caf50;
+      color: white;
+      border: none;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+      border-radius: 0 12px 12px 0;
+
+      &:hover {
+        background-color: #45a049;
+      }
+      &:focus {
+        background-color: #36903b;
+      }
+    }
+  }
+
+  .list {
+    display: flex;
+    padding: 5px;
+
+    &__itim {
+      display: flex;
+      justify-content: space-between;
+      padding: 12px;
+      background-color: #fff;
+      border-radius: 12px;
+      box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.2);
+      width: 100%;
+    }
+
+
+    .delete {
+      background-color: rgb(246, 26, 26);
+      color: white;
+      padding: 5px 10px;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: rgb(196, 44, 44);
+      }
+      &:focus {
+        background-color: rgb(163, 5, 5);
+      }
+    }
+
+    .done {
+      background-color: green;
+      color: white;
+      padding: 5px 10px;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: rgb(86, 161, 86);
+      }
+      &:focus {
+        background-color: rgb(2, 90, 2);
+      }
+    }
+  }
 }
 </style>
