d5d24d45bf71c158afef593e28c31b7e4ee82f55
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-10-16 11:41:43 -1200

    kichik takrorlashlar qo'shildi

--- lesson-5/README.md
+++ lesson-5/README.md
@@ -1,2 +1,3 @@
-# Umumi takrorlash 
+# Umumi takrorlash  va Vue.js da router haqida 
 
+[Projectni run bo'lib turgan holati](https://resonant-centaur-06c915.netlify.app/)
--- lesson-5/src/pages/home/index.vue
+++ lesson-5/src/pages/home/index.vue
@@ -1,60 +1,116 @@
-<script setup></script>
+<script setup>
+const dataLanguage = [
+  {
+    language: "HTML",
+    version: "HTML5",
+    year: 1997,
+    creator: "World Wide Web Consortium",
+    popularity: 90,
+    id: "1dq",
+    image:
+      "https://cdn3d.iconscout.com/3d/free/thumb/free-html-3d-icon-download-in-png-blend-fbx-gltf-file-formats--logo-interface-website-isolated-programming-language-pack-logos-icons-5453024.png?f=webp",
+  },
+  {
+    language: "CSS",
+    version: "CSS3",
+    year: 1996,
+    creator: "Cascading Style Sheets (CSS) Working Group",
+    popularity: 95,
+    id: "2dq",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/011/665/094/non_2x/stylized-3d-css-icon-side-view-free-png.png",
+  },
+  {
+    language: "SCSS",
+    version: "SCSS 3.8",
+    year: 2010,
+    creator: "Sassy CSS Preprocessor",
+    popularity: 90,
+    id: "3dq",
+    image:
+      "https://cdn3d.iconscout.com/3d/free/thumb/free-sass-3d-icon-download-in-png-blend-fbx-gltf-file-formats--html-logo-preprocessor-scripting-language-css-coding-lang-pack-logos-icons-7578019.png?f=webp",
+  },
+  {
+    language: "Bootstrap",
+    version: "Bootstrap 4.6",
+    year: 2019,
+    creator: "Twitter, Inc.",
+    popularity: 95,
+    id: "4dq",
+    image:
+      "https://static.vecteezy.com/system/resources/thumbnails/012/697/297/small/3d-bootstrap-programming-framework-logo-free-png.png",
+  },
+];
+</script>
 <template>
   <section>
     <div class="container">
       <div class="wrapper">
-        <img
-          class="wrapper-image"
-          src="https://r2.erweima.ai/imgcompressed/img/compressed_e80321b4d9602b5cb1f261062635d30b.webp"
-          alt="image"
-        />
-        <div class="wrapper--body">
-          <h1 class="wrapper--body-title">Home page</h1>
-          <p class="wrapper--body-info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt eos rerum,
-            magnam dolor harum laborum similique. Cum amet laudantium laboriosam incidunt
-            ducimus nostrum nesciunt, vero quasi molestiae commodi inventore autem quos
-            eos excepturi labore maxime, dolorum dolore fugit tempore! Exercitationem
-            accusamus quia explicabo nulla tenetur quae omnis libero praesentium ea?
-            Facere ab voluptatum enim repudiandae magnam velit dolor, modi ipsum et!
-            Laboriosam tempore provident ipsam voluptatum fugit culpa adipisci iure
-            eveniet magni. Harum quasi vitae deleniti saepe quibusdam magni numquam fuga
-            quidem fugiat officiis nam debitis error odio pariatur quam sunt magnam,
-            maiores necessitatibus molestias aut voluptatum cupiditate alias quaerat.
-          </p>
-        </div>
+        <ul class="list">
+          <li class="list--itim" v-for="el in dataLanguage" :key="el?.div">
+            <div class="list--itim-image">
+              <img :src="el.image" :alt="el.language" />
+            </div>
+            <div class="list--itim-content">
+              <p>
+                Lenguage: <span>{{ el.language }}</span>
+              </p>
+              <p>
+                Version: <span>{{ el.version }}</span>
+              </p>
+              <p>
+                Year: <span>{{ el.year }} </span>
+              </p>
+              <p>
+                Craator: <span>{{ el.creator }} </span>
+              </p>
+              <p>
+                Popilarity: <span>{{ el.popularity }} </span>
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-.wrapper {
+
+
+
+
+.list {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+  list-style: none;
+}
+
+.list--itim {
+  display: flex;
   align-items: center;
+  gap: 20px;
 }
 
-.wrapper-image {
-  width: 50%;
-  height: auto;
+.list--itim-image > img {
+  width: 150px;
+  height: 150px;
 }
 
-.wrapper--body {
-  width: 50%;
-  padding: 20px;
-}
-
-.wrapper--body-title {
-  font-size: 24px;
+.list--itim-content > p {
+  margin: 0;
+  font-size: 20px;
   font-weight: bold;
-  text-align: center;
-  margin-bottom: 20px;
+  line-height: 1.2;
+  display: flex;
+  align-items: center;
+  gap: 20px;
 }
 
-.wrapper--body-info {
-  text-align: center;
-  line-height: 1.6;
-  margin-bottom: 30px;
+.list--itim-content > p > span {
+  font-weight: normal;
+  color: #4b70ff;
 }
 </style>
