f7001fcca87379f4130ca1869263c6e197921385
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-09 17:34:05 +0500

    no primativ propis testing lesson-4

--- lesson-4/src/App.vue
+++ lesson-4/src/App.vue
@@ -1,12 +1,62 @@
 <template>
- <About :massege="massege"/>
+  <div class="wrapper" >
+
+  <About v-for="(el, i) in dataCard" :dataCardElement="el" />
+  </div>
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
-import About from "./components/Abuot.vue"
+import About from "./components/Abuot.vue";
 
-const massege = ref("vue.js props");
+const dataCard = ref([
+  {
+    id: "1",
+    title: "Title 1",
+    link: "https://picsum.photos/id/123/300/250",
+    alert: "alert text",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem qui nihil officia hic!",
+    activ: false,
+  },
+  {
+    id: "2",
+    title: "Title 2",
+    link: "https://picsum.photos/id/46/300/250",
+    alert: "alert text 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed voluptas qui, quae voluptatibus officiis explicabo?",
+    activ: true,
+  },
+  {
+    id: "3",
+    title: "Title 3",
+    link: "https://picsum.photos/id/459/300/250",
+    alert: "alert text 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quas, perspiciatis sunt, cumque quia, fugit voluptatibus voluptatum ea rerum!",
+    activ: true,
+  },
+  {
+    id: "4",
+    title: "Title 4",
+    link: "https://picsum.photos/id/204/300/250",
+    alert: "alert text 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, cumque, quo veniam error, vitae voluptas expedita voluptatum delectus autem?",
+    activ: false,
+  },
+]);
 </script>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  width: 100%;
+  height: 70vh;
+  padding: 40px 20px;
+  background-color: #f0f0f0;
+}
+</style>
--- lesson-4/src/components/Abuot.vue
+++ lesson-4/src/components/Abuot.vue
@@ -1,17 +1,40 @@
 <template>
-    <div>
-        <h1>{{ massege }}</h1>
-    </div>
+  <div class="wrapper">
+    <img :src="props?.dataCardElement?.link" :alt="dataCardElement?.alert" />
+    <h2>{{ dataCardElement?.title }}</h2>
+    <p>{{ dataCardElement?.description }}</p>
+  </div>
 </template>
 
 <script setup>
+import { defineProps } from "vue";
 const props = defineProps({
-    massege: {
-        type: String,
-    }
-})
+  dataCardElement: {
+    type: Object,
+    required: true,
+    default: () => ({
+      title: "Title 1",
+      link: "https://picsum.photos/id/123/300/250",
+      alert: "alert text",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem qui nihil officia hic!",
+      activ: false,
+    }),
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  max-height: 400px;
+  background-color: #f1f1f1;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+}
 </style>
