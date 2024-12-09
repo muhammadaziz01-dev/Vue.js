a48259d3e83d624ae0ad2de4658b6b0e4f2c8e89
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-09 11:24:20 +0500

    lesson-5 updet and reactiv rout testin

--- lesson-5/src/components/Header.vue
+++ lesson-5/src/components/Header.vue
@@ -6,19 +6,31 @@
           <img src="../assets/vue.svg" alt="Logo" class="nav--logo" />
         </router-link>
         <ul class="nav--list">
-          <router-link to="/" class="nav--list--itim">Home</router-link>
-          <router-link to="/about" class="nav--list--itim">About</router-link>
+          <router-link v-for="(el , index) in navlistArray" :key="index" :to="el.path" class="nav--list--itim">{{ el.name }}</router-link>
+          <!-- <router-link to="/about" class="nav--list--itim">About</router-link>
           <router-link to="/profile" class="nav--list--itim">Profile</router-link>
           <router-link to="/service" class="nav--list--itim">Servises</router-link>
           <router-link to="/signin" class="nav--list--itim">Sign In</router-link>
-          <router-link to="/signup" class="nav--list--itim">Sign Up</router-link>
+          <router-link to="/signup" class="nav--list--itim">Sign Up</router-link> -->
         </ul>
       </nav>
     </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import {ref , reactive } from "vue"
+
+const navlistArray= ref([
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Profile", path: "/profile" },
+  { name: "Services", path: "/service" },
+  { name: "Sign In", path: "/signin" },
+  { name: "Sign Up", path: "/signup" }
+ 
+])
+</script>
 
 <style scoped>
 header {
