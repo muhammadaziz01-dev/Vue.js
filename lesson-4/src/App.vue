396503b910a50ed3d1563c000620449c99964fef
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-09 13:12:29 +0500

    props start lesson-4 project

--- lesson-4/src/App.vue
+++ lesson-4/src/App.vue
@@ -1,40 +1,12 @@
 <template>
-  <div>
-    <h1>Vue.js da dinamic classlar va computedni q'llas</h1>
-    <button @click="handleClick">Handel click</button> 
-    <div>
-      <p :class="[ isSacses ? 'isSacsses' : 'isDanger']">Bu matin dinamic class yordamida , array sintx sida ishlamoqda</p>
-      <p :class="{'isSacsses':isSacses , 'isDanger':!isSacses} ">Bu matin dinamic class yordamida , object sintx sida ishlamoqda</p>
-      <p :class="computedClass">Bu matin computed sintx metodi yordamida ishlamoqda </p>
-
-    </div>
-  </div>
+ <About :massege="massege"/>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';  
-const isSacses = ref(false);
+import { ref, computed } from "vue";
+import About from "./components/Abuot.vue"
 
-const computedClass = computed(() => {
-  return isSacses.value? 'isSacsses' : 'isDanger';
-});
-
-const handleClick = () => {
-  isSacses.value =!isSacses.value;
-};
+const massege = ref("vue.js props");
 </script>
 
-<style  scoped>
-.isSacsses{
-  color: green;
-  font-weight: bold;
-  font-style: italic;
-  font-size: 1.5em;
-}
-.isDanger{
-  color: red;
-  font-weight: bold;
-  font-style: italic;
-  font-size: 1.5em;
-}
-</style>
+<style scoped></style>
---
+++ lesson-4/src/components/Abuot.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ massege }}</h1>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    massege: {
+        type: String,
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
