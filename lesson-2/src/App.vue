074e2a069daa2275b3ca052421ea723b05252b35
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-18 11:19:03 +0500

    lesson-2 cretet watchers

--- lesson-2/src/App.vue
+++ lesson-2/src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div class="container">
-    <h2>{{ firstName }}</h2>
-    <h2>{{ lastName }}</h2>
-    <input v-model="firstName" type="text" placeholder="First Name" />
-    <input v-model="lastName" type="text" placeholder="Last Name" />
+  <div>
+
   </div>
 </template>
 
 <script setup>
-import { ref, watch, watchEffect } from "vue";
 
-const firstName = ref("first name");
-const lastName = ref("last name");
-
-watch(firstName, (newVale, oldVale) => {
-  console.log("old ", oldVale);
-  console.log("new ", newVale);
-});
 </script>
 
-<style lang="css" scoped>
-.container {
-  max-width: 1260px;
-  margin: 0 auto;
-  padding: 0 20px;
-}
+<style lang="scss" scoped>
+
 </style>
---
+++ lesson-2/src/components/watchers.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container" v-memo="lastName">
+      <h2>{{ firstName }}</h2>
+      <h2>{{ lastName }}</h2>
+      <input v-model="firstName" type="text" placeholder="First Name" />
+      <input v-model="lastName" type="text" placeholder="Last Name" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, watch, watchEffect } from "vue";
+  
+  const firstName = ref("first name");
+  const lastName = ref("last name");
+  
+  watch(firstName, (newVale, oldVale) => {
+    // console.log("old ", oldVale);
+    // console.log("new ", newVale);
+  });
+  
+  watchEffect(()=>{
+    console.log("First name and last name combined: ", firstName.value + " " + lastName.value);
+  })
+  
+  </script>
+  
+  <style lang="css" scoped>
+  .container {
+    max-width: 1260px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+  </style>
+  
