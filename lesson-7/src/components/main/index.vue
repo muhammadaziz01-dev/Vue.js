80bc9d041fa89e2bb539e6d225f2e22cab8897ee
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-19 09:38:21 +0500

    lesson-7 pdp unversiti , info section cretet

--- lesson-7/src/components/main/index.vue
+++ lesson-7/src/components/main/index.vue
@@ -1,11 +1,13 @@
 <template>
    <main>
      <Hero/>
+     <InnfoSection title="Bizning kurslarimiz kimga to’g’ri keladi ?" />
    </main> 
 </template>
 
 <script setup>
 import Hero from "../sections/Hero.vue";
+import InnfoSection from "../ui/info-section.vue";
 </script>
 
 <style lang="scss" scoped>
---
+++ lesson-7/src/components/ui/info-section.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="inso-section">
+    <h2>{{ title }}</h2>
+  </section>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: "Default Title",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.inso-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #00b534;
+  padding: 20px 0px 20px;
+
+  h2 {
+    color: #fff;
+    font-size: 33px;
+    font-weight: 800;
+  }
+}
+</style>
