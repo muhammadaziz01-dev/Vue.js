5867735d6b592dab80d13a2a5bce1a402c09d25d
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-19 14:14:16 +0500

    lesson-7 , pdp project location  fix

--- lesson-7/src/components/sections/Location.vue
+++ lesson-7/src/components/sections/Location.vue
@@ -12,7 +12,7 @@
       </div>
       <div class="location--wrrapper--right">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.3081134669105!2d69.23931447587526!3d41.32391297130791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b8582251c2d%3A0x1c2ee4f256515150!2sPDP%20Junior!5e0!3m2!1sru!2s!4v1734599356069!5m2!1sru!2s"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3000.967644243043!2d69.2198547758698!3d41.222475571321326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae6183ea313917%3A0xdc59607ad734d20c!2sPDP%20University!5e0!3m2!1sru!2s!4v1734599567811!5m2!1sru!2s"
           width="100%"
           height="100%"
           style="border: 0"
